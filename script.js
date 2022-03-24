fae943e59ac674aab35c516ae38cfe254fd46b2d
Amanda Portela <mandaptela@gmail.com>
2022-03-24 15:34:44 -0300

    1,2,3 e 4 ok

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function CriarTarefa() {
-  document.getElementsByClassName("button").addEventListener('click');
-  areaTexto = document.getElementByID("input");
-  areaTexto.innerHTML = document.GetElementByID("texto-tarefa").textContent;
+//Adicionar Tarefa
+//acionar botao
+//adicionar item com conteúdo do input
+
+//limpar lista
+//acionar botao
+//remover item selecionado
+
+//limpar tarefas concluidas
+//remover tarefas com classe 'concluida'
+const botoes = document.getElementsByTagName("button");
+const listaTarefas = document.getElementsByClassName("item");
+
+
+for(i=0; i < listaTarefas.length; i +=1){
+  listaTarefas[i].style.color="blue";
+  listaTarefas[3].innerText="Aula ao vivo";
+  listaTarefas[4].innerText="Feedback fim do dia";
+  listaTarefas[5].innerText="Praticar";
 }
 
-  CriarTarefa();
+
