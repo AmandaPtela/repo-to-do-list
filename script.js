8c9e77f2f127ca3e22c8c617f847c35b8dc71565
Amanda Portela <mandaptela@gmail.com>
2022-03-23 23:49:35 -0300

    mudanças visuais

---
+++ script.js
@@ -0,0 +1,7 @@
+function CriarTarefa() {
+  document.getElementsByClassName("button").addEventListener('click');
+  areaTexto = document.getElementByID("input");
+  areaTexto.innerHTML = document.GetElementByID("texto-tarefa").textContent;
+}
+
+  CriarTarefa();
