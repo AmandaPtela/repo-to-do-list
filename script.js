9bca64d7bff7099a80816f78075e985c232a0418
Amanda Portela <mandaptela@gmail.com>
2022-03-27 20:54:28 -0300

    requisito 1,2,3 e 4; metade do 5

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-//Adicionar Tarefa
-//acionar botao
-//adicionar item com conteúdo do input
+//1Título = Minha lista de tarefas
+//2abaixo do título, um paragrafo com id funcionamento e texto = Clique 2 vezes para marcar como completo
+//3input com id texto-tarefa (onde nova tarefa vai ser escrita)
+//4lista ordenada dom id lista-tarefas
+//5botao com id criar-tarefa ; qnd clica adiciona um item na lista e limpa o input
+//6ordenar itens por ordem de criação
+//7 clicar num item muda bg pra cinza rgb(128 128 128)
+//8selecionar apenas 1 item por vez/ mais de um não pode
+//9clicar duas vezes marcar como concluído(riscar item) e clicar duas vezes p desfazer a ação
+//10botao id apaga-tudo limpa a lista
+//11botao remover finalizados (apenas concluidos são apagados)
+//12botao salvar lista. caso a página seja recarregada a lista permanece
+//13botao pra mudar posição do item pra cima (id mover-cima e mover-baixo) e pra baixo na lista
+//14remover selecionado. remover apenas o selecionado
 
-//limpar lista
-//acionar botao
-//remover item selecionado
+const botao = document.querySelector("#criar-tarefa");
+const listaTarefas = document.querySelector("#lista-tarefas");
+const item = ["opa"];
+const areaTexto = document.querySelector("#texto-tarefa");
+const tarefaNova = areaTexto.value;
 
-//limpar tarefas concluidas
-//remover tarefas com classe 'concluida'
-const botoes = document.getElementsByTagName("button");
-const listaTarefas = document.getElementsByClassName("item");
+function criarItemLista(){
+  for(let i=0; i < item.length; i +=1){
+    let novoItem =  item[i];
+     let itemNovo = document.createElement('li');
+      itemNovo.innerText=tarefaNova;
+      listaTarefas.appendChild(itemNovo);
+  } 
+} 
+//adicionar a limpeza do input depois de clicar
+botao.addEventListener("click",criarItemLista) ;
 
-
-for(i=0; i < listaTarefas.length; i +=1){
-  listaTarefas[i].style.color="blue";
-  listaTarefas[3].innerText="Aula ao vivo";
-  listaTarefas[4].innerText="Feedback fim do dia";
-  listaTarefas[5].innerText="Praticar";
-}
-
-
+//
