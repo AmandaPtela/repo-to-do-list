6f36b31135fbe215da9a3c3f5f024ccb3d50b814
Amanda Portela <mandaptela@gmail.com>
2022-03-27 22:12:31 -0300

    tentativa req.6

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //2abaixo do título, um paragrafo com id funcionamento e texto = Clique 2 vezes para marcar como completo
 //3input com id texto-tarefa (onde nova tarefa vai ser escrita)
 //4lista ordenada dom id lista-tarefas
-//5botao com id criar-tarefa ; qnd clica adiciona um item na lista e limpa o input
+
 //6ordenar itens por ordem de criação
-//7 clicar num item muda bg pra cinza rgb(128 128 128)
+
 //8selecionar apenas 1 item por vez/ mais de um não pode
 //9clicar duas vezes marcar como concluído(riscar item) e clicar duas vezes p desfazer a ação
 //10botao id apaga-tudo limpa a lista
@@ -19,15 +19,27 @@ const item = ["opa"];
 const areaTexto = document.querySelector("#texto-tarefa");
 const tarefaNova = areaTexto.value;
 
+
+//5botao com id criar-tarefa ; qnd clica adiciona um item na lista e limpa o input
 function criarItemLista(){
   for(let i=0; i < item.length; i +=1){
     let novoItem =  item[i];
-     let itemNovo = document.createElement('li');
-      itemNovo.innerText=tarefaNova;
-      listaTarefas.appendChild(itemNovo);
+    let itemNovo = document.createElement('li');
+    itemNovo.innerText=tarefaNova;
+    listaTarefas.appendChild(itemNovo);
   } 
 } 
-//adicionar a limpeza do input depois de clicar
+//adicionar a limpeza do input 
 botao.addEventListener("click",criarItemLista) ;
 
+
+//7 clicar num item muda bg pra cinza rgb(128 128 128)
+function mudarbg() {
+
+  for (i = 0; i < listaTarefas.length; i +=1){
+    let bgItem = listaTarefas.childNode;
+    bgItem.style.backgroundColor='gray';
+  }
+}
+itensLista.addEventListener("click", mudarbg)
 //
