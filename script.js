b4c3e18aa60abb653e7b7fee60ab45d76a00ddd0
Amanda Portela <mandaptela@gmail.com>
2022-03-29 14:34:16 -0300

    req 9 completo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 //  8selecionar apenas 1 item por vez/ mais de um não pode
 
-//  11botao remover finalizados (apenas concluidos são apagados)
+
 //  12botao salvar lista. caso a página seja recarregada a lista permanece
 //  13botao pra mudar posição do item pra cima (id mover-cima e mover-baixo) e pra baixo na lista
 //  14remover selecionado. remover apenas o selecionado
@@ -15,6 +15,9 @@ const botaoApagarTudo = document.querySelector('#apaga-tudo');
 const botaoApagarFinalizados = document.querySelector('#remover-finalizados')
 const listaTarefas = document.querySelector('#lista-tarefas');
 const input = document.querySelector('#texto-tarefa');
+const concluidos = document.getElementsByTagName('li');
+const selecionados = document.querySelector('.selected');
+const teclado = KeyboardEvent
 
 // ok 5botao com id criar-tarefa ; qnd clica adiciona um item na lista e limpa o input
 function criarItemLista(ev) {
@@ -23,21 +26,19 @@ function criarItemLista(ev) {
   listaTarefas.appendChild(itemNovo);
   itemNovo.className = 'item';
   input.value = '';
-  ev.target.backgroundColor = 'red';
 } 
 botaoCriar.addEventListener('click', criarItemLista);
 
 // ok 6ordenar itens por ordem de criação
 
 // ok 7 clicar num item muda bg pra cinza rgb(128 128 128)
-function mudarbg(ev) {
+/*function mudarbg(ev) {
   for ( i = 0; i < 1; i += 1); {
     let item = listaTarefas.children[i];
-    ev.target.style.backgroundColor = 'gray';
     ev.target.className = 'selected';
   }
 }
-listaTarefas.addEventListener('click', mudarbg);
+listaTarefas.addEventListener('click', mudarbg);*/
 
 // ok 10 botao id apaga-tudo limpa a lista
 function apagarTudo() {
@@ -48,18 +49,25 @@ function apagarTudo() {
 botaoApagarTudo.addEventListener('click', apagarTudo);
 
 //  9clicar duas vezes marcar como concluído(riscar item) e clicar duas vezes p desfazer a ação
-
+// pesquisei no mdn. Toggle alterna a classe (on/off)
 function concluir(ev) { 
-  ev.target.style.textDecoration = 'line-through solid black';
-  ev.target.className = 'completed';
+  ev.target.classList.toggle('completed');
 }
 listaTarefas.addEventListener('dblclick', concluir);
 
-function apagarSelecionados() {}
+//function desfazerConcluir(ev) { 
+//  ev.target.classList.remove('completed');
+//}
+//listaTarefas.addEventListener('click', desfazerConcluir);
+
+//  11botao remover finalizados (apenas concluidos são apagados)
+function apagarFinalizados() {
+}
+botaoApagarFinalizados.addEventListener('click', apagarFinalizados)
 
 
 function salvarLista(params) {
-  
 }
+
 function removerselecionado() {
-os.addEventListener('click', removerselecionado);}
+}
